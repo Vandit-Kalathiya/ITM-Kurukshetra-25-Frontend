8c74b3160b51a2e069a4cd72f1c692eced693834
Suhasi <suhasikakani545@gmail.com>
2025-03-26 15:22:12 +0530

    Configured all required configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster as Sonner } from "./Component/CropAdvisoryBot/components/ui/Sonner.jsx";
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
+import { TooltipProvider } from './Component/CropAdvisoryBot/components/ui/Tooltip.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <TooltipProvider>
+      <Toaster/>
+      <Sonner />
+      <App />
+    </TooltipProvider>
+  </BrowserRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  // console.log(env); 
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: env.VITE_PORT,
+    },
+  };
+});
